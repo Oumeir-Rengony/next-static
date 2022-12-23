7d5abf4628a3a3b23cb7fb7642aa397329fd72b4
Mohammad Oumeir Rengony <oumeir.rengony@rapp.com>
2022-12-23 12:20:05 +0400

    static 2

--- _next/static/chunks/pages/_error-8b9f5cffb5c7c409.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[4820],{81981:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_error",function(){return u(89185)}])}},function(n){n.O(0,[9774,3950,2888,2716,179],(function(){return _=81981,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
