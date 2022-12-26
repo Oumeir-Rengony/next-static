0b4f64fb01f967b251d9e6fbbae4fd4b17d4f7d9
Mohammad Oumeir Rengony <oumeir.rengony@rapp.com>
2022-12-27 01:28:56 +0400

    static web worker

--- _next/static/trASj0M-z044FB-GFUOr1/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002F[...slug]","\u002Fcustom404","\u002Fcustom500","\u002Fguides\u002Flanding\u002F[...path]","\u002Fmaintenance","\u002Fsitemap"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
